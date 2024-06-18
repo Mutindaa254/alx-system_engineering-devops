7d0efeea970179061e85a59654ea10f8fc618b53
Moses Mumo <mmmutinda@kabarak.ac.ke>
2024-06-18 20:21:04 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
-import requests
-def top_ten(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, print None.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
+""" Queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit. """
 
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
+import requests
+headers = {"User-Agent": "Custom"}
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    request = requests.get(url, headers=headers, allow_redirects=False)
+
+    if request.status_code == 200:
+        for children in request.json().get("data").get("children"):
+            print(children.get("data").get("title"))
+
     else:
         print(None)
